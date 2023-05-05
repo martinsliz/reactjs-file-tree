4a0d57f4d0c07237464546c1a0a1cdf0a34a956e
Liz Martins <elizabeth.martins5@gmail.com>
2023-05-05 11:43:03 -0400

    folder and files created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Folder name="Desktop">
+        <File name="dogs.jpg" />
+        <File name="cats.png" />
+      </Folder>
+      <Folder name="Applications" />
     </div>
   )
+}
+
+const Folder = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h3>{props.name}</h3>
+      <div style={{ marginLeft: '17px' }}>
+        <h4>{props.children}</h4>
+      </div>
+    </div>
+  )
+}
+
+const File = (props) => {
+  return <div>{props.name}</div>
 }
 
 export default App
